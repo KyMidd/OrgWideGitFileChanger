3df50dda464180fc5427bc13cf24cba4a2576359
Kyler Middleton <kyler.middleton@practicefusion.com>
2023-03-21 15:52:58 -0500

    Init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # OrgWideGitFileChanger
-A bash-based tool to read over all GitHub Org's repos, clone them all, and make changes to each one in sequence. Linked to Medium article: 
+
+A bash-based tool to read over all GitHub Org's repos, clone them all, and make changes to each one in sequence. Linked to Medium article: https://kymidd.medium.com/lets-do-devops-update-files-in-hundreds-of-github-repos-5262dfe5f529
---
+++ org_wide_git_changer.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# Summary
+# This tool is intended to loop over every repo within an Org or User space, and make changes to the repo.
+# It's useful for when you need to make a change to every repo in an org
+
+# Auth Requirements
+# Make sure to export your github token. If SSO is enabled in your Org, you will need to authorize your token for SSO within the Org
+# export GITHUB_TOKEN='ghp_xxxx'
+
+# check to make sure GITHUB_TOKEN is set
+if [ -z "$GITHUB_TOKEN" ]; then 
+  echo '$GITHUB_TOKEN is not set, please set it and try again'
+  exit 0
+fi
+
+# Set vars
+gh_org=your-org-name-here # Your GitHub Organization (or your username, if that's where your repos are)
+
+# PR information - please customize this information
+pr_body="This PR makes some automated changes to the repo."
+pr_title="🤖 Making some changes 🤖"
+branch_name='Branch-Name-Here'
+commit_message='Commit message here'
+
+# Should we use admin privileges to merge PR. 
+# If true, admin privileges will be used to merge the PR. You must have admin privileges to use this option. 
+# If false, the PR will not be automatically merged. The URL will be written to the log, and you must merge them manually
+auto_merge_pr=false
+
+# Get the names of all repos in the org
+# This method is limited to 1k repos, if you have more than 1k repos, use this method: https://medium.com/@kymidd/lets-do-devops-github-api-paginated-calls-more-than-1k-repos-3ff0cc92cc50
+org_repos=$(gh repo list --no-archived $gh_org -L 1000 --json name --jq '.[].name')
+
+# Iterate over all repos, make changes
+while IFS=$'\n' read -r gh_repo; do
+  
+  # Clone the repo, will fail if the repo folder already exists
+  git clone git@github.com:$gh_org/${gh_repo}.git
+
+  # Change directories into the repo
+  cd $gh_repo
+
+  ###
+  ### Make your changes here
+  ### Add or delete any files you need to this location
+  ### For example, modify any file, or copy over existing files
+  ###
+
+  # Read the REST info on the repo to get the repo's default branch
+  # Set that default branch as the base branch for the PR
+  base_branch=$(curl -s \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $GITHUB_TOKEN" \
+    https://api.github.com/repos/$gh_org/$gh_repo | jq -r '.default_branch')
+  
+  # Git add with '.' target identifies all changes
+  # Using the '-vvv' verbose flag to get the output of the git add command, which we will use to determine if there are changes
+  git_add=$(git add -vvv .)
+  
+  # If there are no changes, the PR will not be created
+  # Note that even modified files will show up as 'add' in the git add output
+  if [[ $(echo "$git_add" | grep -E 'add|remove') ]]; then
+    
+    # Changes were made, checkout a branch and make a PR
+    git checkout -b $branch_name
+    git commit -m $commit_message
+    ggpush
+    created_pr_url=$(gh pr create -b "$pr_body" -t "$pr_title" -B "$base_branch" --fill)
+
+    # If auto_merge_pr is true, merge the PR
+    if [ "$auto_merge_pr" = true ]; then
+      gh pr merge --admin -d -m $created_pr_url
+    else
+      echo "PR created, please merge: $created_pr_url"
+    fi
+
+    # Sleep 2 seconds to avoid rate limiting
+    sleep 2
+
+  fi
+
+  # Reset location
+  cd ..
+
+done <<< "$org_repos"
+
+# Finish
+echo "################"
+echo "Done!"
+echo "################"
+exit 0
